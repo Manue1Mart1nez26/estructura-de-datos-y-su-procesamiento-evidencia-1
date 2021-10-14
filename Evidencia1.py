38c742630910a8de61798e06bf0ae6333b83b778
Manuel Martínez <92546608+Manue1Mart1nez26@users.noreply.github.com>
2021-10-14 14:41:54 -0500

    Create Evidencia1.py

---
+++ Evidencia1.py
@@ -0,0 +1,11 @@
+#prueba
+print('FELIZ NAVIDAD!!!!!')
+print('')
+for i in range(1,20,2):
+    print(('*'*i).center(20))
+
+for leg in range(1):
+    print(('|||').center(20))
+    print(('\_____/').center(20))
+    print(''+20*'-')
+    
