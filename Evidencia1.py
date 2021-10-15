ab0d7839a2eea0d972fc08e6e0f32b55961d6648
Manuel Martínez <92546608+Manue1Mart1nez26@users.noreply.github.com>
2021-10-15 16:06:16 -0500

    Update Evidencia1.py

--- Evidencia1.py
+++ Evidencia1.py
@@ -2,14 +2,16 @@
 Evidencia 1, Estructura de datos y su procesamiento.
 
 Carlos Manuel Martínez Martínez."""
-
+import pandas as pd
 from collections import namedtuple
 from os import remove
 
-Ventas = namedtuple("Ventas",["nombre","correoElectronico"])
+SEPARADOR = ("*" * 20)
+Ventas = namedtuple("Ventas",["Articulo","FechaVenta"])
 DiccionarioVentas = {}
-DiccionarioPrecios = {"Programación":[87, 80, 80,20,100], "Base de datos":[71, 100, 70, 100, 100],
-"Contabilidad":[99, 57, 57, 100, 100]}
+DiccionarioPrecios = {"Juego de llantas 1":[400], "Juego de llantas 2":[600]}
+notas_Precios = pd.DataFrame(DiccionarioPrecios)
+
 
 while True:
     print("\n-- Bienvenido(a) al Menú")
@@ -22,67 +24,58 @@ while True:
     opcionElegida = int(input("> "))
 
     if opcionElegida == 1: #Lista o menu con los articulos y precios que se visualiza| Cambiar para que se visualize en lo que hay en el DiccionarioPrecios (also cambiar los datos dentro del diccionario)
-        while True:
-            clave = int(input("Porfavor ingrese la clave de la Venta: "))
-            if clave in DiccionarioVentas.keys():
-                print("Ya existe en el diccionario esa clave, intente nuevamente")
-            else:
-                nombre = input("Porfavor ingrese su nombre: ").capitalize()
-                correoElectronico = input("Porfavor introduzca su correo electrónico: ").lower()
-                TuplaVenta = Ventas(nombre,correoElectronico)
-                DiccionarioVentas[clave] = TuplaVenta
-                print(f"\n-- Confirmación de datos:\nClave: {clave}, Nombre: {nombre}, Correo: {correoElectronico}")
-                break
+        if DiccionarioPrecios:
+            print(notas_Precios.mean())
 
-    if opcionElegida == 2:#Registrar una venta| 2.2 dentro de este que agregue mas articulos y no solo uno
+    if opcionElegida == 2: #Registrar una venta| 2.2 dentro de este que agregue mas articulos y no solo uno
         while True:
-            clave = int(input("Porfavor ingrese la clave de la Venta: "))
-            if clave in DiccionarioVentas.keys():
-                print("Ya existe en el diccionario esa clave, intente nuevamente")
+            folioUnico = int(input("Porfavor ingrese el numero de venta : "))
+            if folioUnico in DiccionarioVentas.keys():
+                print("Ya existe en el diccionario esa folioUnico, intente nuevamente")
             else:
-                nombre = input("Porfavor ingrese su nombre: ").capitalize()
-                correoElectronico = input("Porfavor introduzca su correo electrónico: ").lower()
-                TuplaVenta = Ventas(nombre,correoElectronico)
-                DiccionarioVentas[clave] = TuplaVenta
-                print(f"\n-- Confirmación de datos:\nClave: {clave}, Nombre: {nombre}, Correo: {correoElectronico}")
+                Articulo = input("Porfavor ingrese su Articulo: ").capitalize()
+                FechaVenta = input("Porfavor introduzca la fecha: ").lower()
+                TuplaVenta = Ventas(Articulo,FechaVenta)
+                DiccionarioVentas[folioUnico] = TuplaVenta
+                print(f"\n-- Confirmación de datos:\nfolioUnico: {folioUnico}, Articulo: {Articulo}, Fecha: {FechaVenta}")
                 break
 
     if opcionElegida == 3:#Consultar una venta
         if DiccionarioVentas:
-            claveBuscado = int(input("Ingrese la clave a buscar: "))
-            if claveBuscado in DiccionarioVentas:
+            folioUnicoBuscado = int(input("Ingrese La venta a buscar: "))
+            if folioUnicoBuscado in DiccionarioVentas:
                 print("\n-- Resultado de búsqueda:")
-                print(f"Nombre: {DiccionarioVentas[claveBuscado].nombre}")
-                print(f"Correo: {DiccionarioVentas[claveBuscado].correoElectronico}")
+                print(f"Articulo: {DiccionarioVentas[folioUnicoBuscado].Articulo}")
+                print(f"Fecha: {DiccionarioVentas[folioUnicoBuscado].FechaVenta}")
             else:
-                print("No existe la clave introducida, intente nuevamente")
+                print("No existe La venta introducida, intente nuevamente")
 
     if opcionElegida == 4:#Comprobacion de que estan las ventas
         if DiccionarioVentas:
             print("\n-- Listado completo de Ventas")
-            print(f'\n{"Clave":<5} | {"Nombre":^18} | {"Correo":<25}')
-            for claveCiclo in DiccionarioVentas.keys():
-                print(f'{claveCiclo:<5} | {DiccionarioVentas[claveCiclo].nombre:^18} | {DiccionarioVentas[claveCiclo].correoElectronico:<25}')
+            print(f'\n{"folioUnico":<10} | {"Articulo":^18} | {"Fecha":<25}')
+            for folioUnicoCiclo in DiccionarioVentas.keys():
+                print(f'{folioUnicoCiclo:<10} | {DiccionarioVentas[folioUnicoCiclo].Articulo:^18} | {DiccionarioVentas[folioUnicoCiclo].FechaVenta:<25}')
         else:
             print("No se encuentra ningún registro")
 
     if opcionElegida == 5:#Eliminacion de ventas
-        claveEliminar = int(input('Clave del registro a eliminar: '))
+        folioUnicoEliminar = int(input('folioUnico del registro a eliminar: '))
 
-        if claveEliminar in DiccionarioVentas.keys():
+        if folioUnicoEliminar in DiccionarioVentas.keys():
             print('\n-- Registor a eliminar:')
-            print(f'\n{"Clave":<5} | {"Nombre":^18} | {"Correo":<25}')
-            print(f'{claveEliminar:<5} | {DiccionarioVentas[claveEliminar].nombre:^18} | {DiccionarioVentas[claveEliminar].correoElectronico:<25}')
+            print(f'\n{"folioUnico":<10} | {"Articulo":^18} | {"Fecha":<25}')
+            print(f'{folioUnicoEliminar:<10} | {DiccionarioVentas[folioUnicoEliminar].Articulo:^18} | {DiccionarioVentas[folioUnicoEliminar].FechaVenta:<25}')
             eliminacion_reg = int(input("\n¿Está seguro de eliminar este registro?\n1) Si\n2) No\n> "))
             if eliminacion_reg == 1:
-                del DiccionarioVentas[claveEliminar]
-                print(f"Se ha eliminado satisfactoriamente al registro No. {claveEliminar}")
+                del DiccionarioVentas[folioUnicoEliminar]
+                print(f"Se ha eliminado satisfactoriamente al registro No. {folioUnicoEliminar}")
             elif eliminacion_reg == 2:
                 print("No se eliminará el registro")
             else:
                 print("El dato es inválido")
         else:
-            print("No se encuentra la clave introducida")
+            print("No se encuentra La venta introducida")
 
     if opcionElegida == 6:
         print("Gracias por usar el programa, buen día.")
